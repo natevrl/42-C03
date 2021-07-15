69398130789422f20bec41f2e0fe527f86135e50
Nathan BENHADOUCHE <nbenhado@e2r7p1.42.fr>
2021-07-15 15:57:54 +0200

    commencer le strlcat pour repush

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,24 +6,9 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/04 17:24:26 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/07/06 18:22:17 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/07/06 22:10:25 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_strcmp(char	*s1, char	*s2)
-{
-	while (*s1)
-	{
-		if (*s1 == *s2)
-		{
-			s1++;
-			s2++;
-		}
-		else
-			break ;
-	}
-	return (*s1 - *s2);
-}
 
 int	ft_strlen(char	*str)
 {
@@ -41,26 +26,40 @@ char	*ft_strstr(char	*str, char	*to_find)
 {
 	int	i;
 	int	max;
+	int	j;
 
 	i = 0;
 	max = ft_strlen(to_find);
-	if (ft_strlen(to_find) > ft_strlen(str))
-		return (0);
 	if (*to_find == '\0')
 		return (str);
 	while (str[i] != '\0')
 	{
 		if (str[i] == to_find[0])
 		{
-			while (str[i] == *to_find)
+			j = 0;
+			while (str[i] == to_find[j])
 			{
+				if (j == max - 1)
+					return (str + (i - j));
 				i++;
-				to_find++;
-				if (*to_find == '\0')
-					return (&str[i - max]);
+				j++;
 			}
 		}
-		i++;
+		else
+			i++;
 	}
 	return (0);
+}
+
+
+#include <string.h>
+#include <stdio.h>
+
+
+int	main(void)
+{
+	printf("\t\tEx04: \n");
+	
+	printf("Test de ft_strstr sur '', '' : %s\n", ft_strstr("awdawdabawdawdabcKK", "abc"));
+	printf("Test de strstr sur '', '' : %s\n", strstr("awdawdabawdawdabcKK", "abc"));
 }
